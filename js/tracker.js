4d1dce206e74f174c5acb1c56192992f42a80a53
prashant1282 <83161625+prashant1282@users.noreply.github.com>
2026-01-29 15:06:37 +0530

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -2,5 +2,3 @@
 // Add your session replay SDK, analytics, or tracking code here
 
 (function(u,z,a){if(u["a_id"]){return;}u["a_id"]=a;u["dm"]=u.location.hostname;u.uzera=u.uzera||[];u.uzera.q=u.uzera.q||[];['identify','trackEvent'].forEach(function(m){u.uzera[m]=function(){u.uzera.q.push([m,arguments]);};});var s=z.createElement("script");s.async=true;s.src="https://assets.uzera.com/wrapper.min.js";var t=z.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);})(window,document,"311G77LNMW");
-  console.log('Tracker loaded on:');
-
