dd435bbdc6a8a59b6a493748ddaa3e7f85df0bed
prashant1282 <83161625+prashant1282@users.noreply.github.com>
2026-02-02 18:05:47 +0530

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -1,5 +1,4 @@
 // Tracker Script - Loads on every page
 // Add your session replay SDK, analytics, or tracking code here
 
-(function(u,z,a){if(u["a_id"]){return;}u["a_id"]=a;u["dm"]=u.location.hostname;u.uzera=u.uzera||[];u.uzera.q=u.uzera.q||[];['identify','trackEvent'].forEach(function(m){u.uzera[m]=function(){u.uzera.q.push([m,arguments]);};});var s=z.createElement("script");s.async=true;s.src="https://assets.uzera.com/wrapper.min.js";var t=z.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);})(window,document,"311G77LNMW");
- 
+(function(u,z,a){if(u["a_id"]){return;}u["a_id"]=a;u["dm"]=u.location.hostname;u.uzera=u.uzera||[];u.uzera.q=u.uzera.q||[];['identify','trackEvent'].forEach(function(m){u.uzera[m]=function(){u.uzera.q.push([m,arguments]);};});var s=z.createElement("script");s.async=true;s.src="https://assets.uzera.com/wrapper.min.js";var t=z.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);})(window,document,"4ZDJ4CQ4DD");
